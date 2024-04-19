d18b3c82b113fe8982e1f1ed60b0dbf9f46611d4
Yaroslav Tashlanov <yarik.tashlanov@mail.ru>
2024-04-19 10:31:14 +0500

    correct deficiencies

--- reacttetApp/src/App.jsx
+++ reacttetApp/src/App.jsx
@@ -1,7 +1,7 @@
 import faceImage from './assets/faceImage.jpg'
-import Comm from './assets/communication.svg'
-import Fork from './assets/office.svg'
-import Problem from './assets/problem.svg'
+import communicationSvg from './assets/communication.svg'
+import officeSvg from './assets/office.svg'
+import problemSvg from './assets/problem.svg'
 import Business from './assets/business.svg'
 import { Card } from './components/card/card'
 import './App.css'
@@ -9,25 +9,25 @@ import './App.css'
 function App() {
   return (
     <> 
-      <div className='mainCard'>
-        <Card src={faceImage} class='imageFace'/>
+      <div className="mainCard">
+        <Card src={faceImage} class="imageFace" alt="Фотография Ташланова Ярослава"/>
         <p>Ташланов Ярослав</p>
       </div>
-      <div className='forCard'>
-        <div className='for'>
-          <Card src={Comm} class='imageFace'/>
+      <div className="forCard">
+        <div className="for">
+          <Card src={communicationSvg} class="imageFace" alt="Коммуникации"/>
           <p>Коммуникация ― способность находить общий язык с разными типажами людей и в разных ситуациях.</p>
         </div>
-        <div className='for'>
-          <Card src={Fork} class='imageFace'/>
+        <div className="for">
+          <Card src={officeSvg} class="imageFace" alt="Командная работа"/>
           <p>Командная работа ― способность влиться в команду, работать вместе и помогать другим участникам.</p>
         </div>
-        <div className='for'>
-          <Card src={Problem} class='imageFace'/>
+        <div className="for">
+          <Card src={problemSvg} class="imageFace" alt="Умение решать проблемы"/>
           <p>Problem solving ― умение решать проблемы и находить выход из сложных ситуаций.</p>
         </div>
-        <div className='for'>
-          <Card src={Business} class='imageFace'/>
+        <div className="for">
+          <Card src={BusinebusinessSvg} class="imageFace" alt="Ориеентация на результат"/>
           <p>Ориентация на результат ― способность работать не ради процесса, а ради результата.</p>
         </div>
       </div>
@@ -63,7 +63,6 @@ function App() {
           <p>Docer</p>
         </div>
       </div>
-      
     </>
   )
 }
--- reacttetApp/src/components/card/card.jsx
+++ reacttetApp/src/components/card/card.jsx
@@ -1,14 +1,11 @@
-import faceImage from '../../assets/faceImage.jpg'
 import './style.css'
 
 export function Card(props) {
     return (
-        <>
             <div className={props.className}>
-                <img src={props.src} class={props.class}/>
+                <img src={props.src} class={props.class} alt={props.alt}/>
                 <p>{props.text}</p>
             </div>
-        </>
     )
 }
 
